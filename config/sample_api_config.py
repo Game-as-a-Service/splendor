61c65b132556d5e39cbfe114f51bbc06dcbef095
Eugene.Hsu <eugene.hsu@leadinfo.com.tw>
2023-04-07 11:49:34 +0800

    rm3

--- config/sample_api_config.py
+++ config/sample_api_config.py
@@ -5,24 +5,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    APP_NAME = "Nike-Api-Flask"
+    APP_NAME = "Gem"
     ENV_SET = "Local"
     DEBUG = True
 
     JSON_AS_ASCII = False
-    SECRET_KEY = "Nike Flask Secret"
-
-    # Growin Url
-    GROWIN_URL = "https://127.0.0.1/"
-
-    # Growin Mail Server
-    GROWIN_MAIL_URL = "https://127.0.0.1/"
+    SECRET_KEY = "Secret"
 
     # log setting
     LOG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'logs'))
-    LOG_PATH_ERROR = os.path.join(LOG_PATH, 'nike_error.log')
-    LOG_PATH_INFO = os.path.join(LOG_PATH, 'nike_info.log')
-    LOG_PATH_DEBUG = os.path.join(LOG_PATH, 'nike_debug.log')
+    LOG_PATH_ERROR = os.path.join(LOG_PATH, 'gem_error.log')
+    LOG_PATH_INFO = os.path.join(LOG_PATH, 'gem_info.log')
+    LOG_PATH_DEBUG = os.path.join(LOG_PATH, 'gem_debug.log')
     LOG_FILE_MAX_BYTES = 100 * 1024 * 1024
     LOG_FILE_BACKUP_COUNT = 10
 
@@ -39,12 +33,6 @@ class Config:
     CRONJOB_PWD = ""
 
     # MySQL and SQLAlchemy Setting
-    # Manager DB
-    MANAGER_DB_HOST = "127.0.0.1"
-    MANAGER_DB_PORT = 3306
-    MANAGER_DB_USER = "root"
-    MANAGER_DB_PWD = "secret"
-    MANAGER_DB_NAME = "tradingvalley_manager"
 
     # User Profile DB
     USER_DB_HOST = "127.0.0.1"
@@ -53,40 +41,7 @@ class Config:
     USER_DB_PWD = "secret"
     USER_DB_NAME = "user_profile"
 
-    # Nike DB
-    NIKE_DB_HOST = "127.0.0.1"
-    NIKE_DB_PORT = 3306
-    NIKE_DB_USER = "root"
-    NIKE_DB_PWD = "secret"
-    NIKE_DB_NAME = "nike"
-
-    # Symbol DB
-    SYMBOL_DB_HOST = "127.0.0.1"
-    SYMBOL_DB_PORT = 3306
-    SYMBOL_DB_USER = "root"
-    SYMBOL_DB_PWD = "secret"
-    SYMBOL_DB_NAME = "symboldb"
-
-    # SEC13F DB
-    SEC13F_DB_HOST = "127.0.0.1"
-    SEC13F_DB_PORT = 3306
-    SEC13F_DB_USER = "root"
-    SEC13F_DB_PWD = "secret"
-    SEC13F_DB_NAME = "SEC13Fdb"
-
-    # Notification DB
-    NOTICE_DB_HOST = "127.0.0.1"
-    NOTICE_DB_PORT = 3306
-    NOTICE_DB_USER = "root"
-    NOTICE_DB_PWD = "secret"
-    NOTICE_DB_NAME = "push_notification"
-
-    MANAGER_DATABASE_URI = f"mysql+pymysql://{MANAGER_DB_USER}:{MANAGER_DB_PWD}@{MANAGER_DB_HOST}:{MANAGER_DB_PORT}/{MANAGER_DB_NAME}?charset=utf8mb4"  # noqa: E501
     USER_DATABASE_URI = f"mysql+pymysql://{USER_DB_USER}:{USER_DB_PWD}@{USER_DB_HOST}:{USER_DB_PORT}/{USER_DB_NAME}?charset=utf8mb4"  # noqa: E501
-    NIKE_DATABASE_URI = f"mysql+pymysql://{NIKE_DB_USER}:{NIKE_DB_PWD}@{NIKE_DB_HOST}:{NIKE_DB_PORT}/{NIKE_DB_NAME}?charset=utf8mb4"  # noqa: E501
-    SYMBOL_DATABASE_URI = f"mysql+pymysql://{SYMBOL_DB_USER}:{SYMBOL_DB_PWD}@{SYMBOL_DB_HOST}:{SYMBOL_DB_PORT}/{SYMBOL_DB_NAME}?charset=utf8mb4"  # noqa: E501
-    SEC13F_DATABASE_URI = f"mysql+pymysql://{SEC13F_DB_USER}:{SEC13F_DB_PWD}@{SEC13F_DB_HOST}:{SEC13F_DB_PORT}/{SEC13F_DB_NAME}?charset=utf8mb4"  # noqa: E501
-    NOTICE_DATABASE_URI = f"mysql+pymysql://{NOTICE_DB_USER}:{NOTICE_DB_PWD}@{NOTICE_DB_HOST}:{NOTICE_DB_PORT}/{NOTICE_DB_NAME}?charset=utf8mb4"  # noqa: E501
     SQLALCHEMY_POOL_SIZE = 20
     SQLALCHEMY_MAX_OVERFLOW = 10
     SQLALCHEMY_POOL_TIMEOUT = 30
