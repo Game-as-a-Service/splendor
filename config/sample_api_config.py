d309f287f4c822cba42cb3b72f5e037a69768806
I-Chun Hsu <a13579230@gmail.com>
2023-04-23 20:48:40 +0800

    happycase

--- config/sample_api_config.py
+++ config/sample_api_config.py
@@ -13,10 +13,10 @@ class Config:
     SECRET_KEY = "Secret"
 
     # log setting
-    LOG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'logs'))
-    LOG_PATH_ERROR = os.path.join(LOG_PATH, 'gem_error.log')
-    LOG_PATH_INFO = os.path.join(LOG_PATH, 'gem_info.log')
-    LOG_PATH_DEBUG = os.path.join(LOG_PATH, 'gem_debug.log')
+    LOG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "logs"))
+    LOG_PATH_ERROR = os.path.join(LOG_PATH, "gem_error.log")
+    LOG_PATH_INFO = os.path.join(LOG_PATH, "gem_info.log")
+    LOG_PATH_DEBUG = os.path.join(LOG_PATH, "gem_debug.log")
     LOG_FILE_MAX_BYTES = 100 * 1024 * 1024
     LOG_FILE_BACKUP_COUNT = 10
 
@@ -24,7 +24,7 @@ class Config:
     LOG_DISCORD_HOOK_URL = "https://discord.com/api/webhooks/"
     LOG_DISCORD_LEVEL = "WARNING"
 
-    # For Discord register TradingView Indicator Channel
+    # For Discord Channel
     DISCORD_TRADINGVIEW_HOOK_URL = "https://discord.com/api/webhooks/"
 
     # CRONJOB SERVER
@@ -34,12 +34,12 @@ class Config:
 
     # MySQL and SQLAlchemy Setting
 
-    # User Profile DB
+    # Gem DB
     USER_DB_HOST = "127.0.0.1"
     USER_DB_PORT = 3306
     USER_DB_USER = "root"
     USER_DB_PWD = "secret"
-    USER_DB_NAME = "user_profile"
+    USER_DB_NAME = "gem"
 
     USER_DATABASE_URI = f"mysql+pymysql://{USER_DB_USER}:{USER_DB_PWD}@{USER_DB_HOST}:{USER_DB_PORT}/{USER_DB_NAME}?charset=utf8mb4"  # noqa: E501
     SQLALCHEMY_POOL_SIZE = 20
@@ -54,9 +54,9 @@ class Config:
     SESSION_COOKIE_SECURE = False
     SESSION_COOKIE_HTTPONLY = True
     SESSION_KEY_PREFIX = f"{ENV_SET}-gem-session:"
-    SESSION_REDIS_HOST = 'localhost'
+    SESSION_REDIS_HOST = "localhost"
     SESSION_REDIS_PASSWORD = "secret"
-    SESSION_REDIS_PORT = '6379'
+    SESSION_REDIS_PORT = "6379"
     SESSION_REDIS_DB = 9
     SESSION_REDIS_SSL = False
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=100)  # 配置10分鐘有效
@@ -70,9 +70,7 @@ class Config:
     CACHE_DEFAULT_TIMEOUT = 4320
     CACHE_REDIS_DB = 10
     CACHE_THRESHOLD = 922337203685477580
-    CACHE_OPTIONS = {
-        'ssl': bool(SESSION_REDIS_SSL)
-    }
+    CACHE_OPTIONS = {"ssl": bool(SESSION_REDIS_SSL)}
 
     # Stripe Setting
     STRIPE_PUBLISHABLE_KEY = ""
