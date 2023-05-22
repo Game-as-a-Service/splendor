429817cb265d541638e2c3d431bab26145cf916c
noracami <kerke.2011@gmail.com>
2023-05-23 03:24:23 +0800

    pass TDD

--- domain/game.py
+++ domain/game.py
@@ -3,6 +3,7 @@ import uuid
 from enum import Enum
 from .table import Table
 from .player import Player
+from .resource import Resource, Token
 from typing import List
 
 
@@ -22,6 +23,21 @@ class Game:
 
     def end_game(self) -> None:
         pass
+
+    def get_token(self, player: Player, res: Resource):
+        # player.resource.appendToken(res.token)
+        player.resource.token.extend(res.token)
+        # self.table.resource.removeToken(res.token)
+        for x in res.token:
+            self.table.resource.token.remove(Token.__members__[x.name])
+        player.resource = self.tokens_to_resource(player.resource.token)
+        self.table.resource = self.tokens_to_resource(self.table.resource.token)
+
+    def tokens_to_resource(self, tokens: list[Token]) -> Resource:
+        ret = Resource()
+        for token in tokens:
+            ret.__setattr__(token.name, ret.__getattribute__(token.name) + 1)
+        return ret
 
 
 class Status(Enum):
--- tests/get_token/test_get_three_different_tokens.py
+++ tests/get_token/test_get_three_different_tokens.py
@@ -1,7 +1,8 @@
 from domain.game import Game
 from domain.player import Player
-from domain.table import Table
-from domain.resource import Resource
+
+# from domain.table import Table
+from domain.resource import Resource, Token
 
 
 from tests.base_flask_test_case import BaseFlaskTestCase
@@ -25,17 +26,34 @@ class TestGetThreeDifferentTokens(BaseFlaskTestCase):
     def tearDown(self) -> None:
         return super().tearDown()
 
-    def test_get_three_different_tokens_case01(self):
+    def test_get_three_different_tokens_case01(self) -> None:
         """happy path: successfully taken 3 different tokens"""
 
         # given
         game = Game()
-        player_a = Player(name="player A")
-        game.add_player(player_a)
-        game.table = Table(Resource(diamond=2, sapphire=4, emerald=3))
+        player_a = Player()
+        # FIXME: avoid direct access instance variable
+        # game.add_player(player_a)
+        game.players = [player_a]
+        game.table.resource.diamond = 2
+        game.table.resource.sapphire = 4
+        game.table.resource.emerald = 3
+        # game.table.resource.token = [Token.diamond, Token.diamond, Token.sapphire, Token.sapphire, Token.sapphire, Token.sapphire, Token.emerald, Token.emerald, Token.emerald]
+        game.table.resource.token = self.resource_to_tokens(game.table.resource)
+        # for token in Token.__members__:
+        #     token_counts = game.table.resource.__getattribute__(token)
+        #     if token_counts > 0:
+        #         for i in range(token_counts):
+        #             game.table.resource.token.append(Token.__members__[token])
 
         # when
-        game.get_token(player_a, Resource(diamond=1, sapphire=1, emerald=1))
+        taken_resource = Resource()
+        taken_resource.diamond = 1
+        taken_resource.sapphire = 1
+        taken_resource.emerald = 1
+        taken_resource.token = self.resource_to_tokens(taken_resource)
+
+        game.get_token(player_a, taken_resource)
 
         # then
         self.assertEqual(player_a.resource.diamond, 1)
@@ -44,3 +62,12 @@ class TestGetThreeDifferentTokens(BaseFlaskTestCase):
         self.assertEqual(game.table.resource.diamond, 1)
         self.assertEqual(game.table.resource.sapphire, 3)
         self.assertEqual(game.table.resource.emerald, 2)
+
+    def resource_to_tokens(self, resource: Resource) -> list[Token]:
+        tokens = []
+        for token in Token.__members__:
+            token_counts = resource.__getattribute__(token)
+            if token_counts > 0:
+                for i in range(token_counts):
+                    tokens.append(Token.__members__[token])
+        return tokens
